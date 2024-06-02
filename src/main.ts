c9e0f587700a30b2a0eb5c3f9c0e13c49d0274bf
Tobias <sirianni.gf@gmail.com>
2024-06-02 11:35:26 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  await app.listen(4000);
+}
+bootstrap();
