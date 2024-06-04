c0df18c3e9ac9d95f6bc0053cdb6ea0efdae8224
Tobias <sirianni.gf@gmail.com>
2024-06-04 17:34:25 -0300

    chore: add simple validation of .env file for development purposes

--- src/init.ts
+++ src/init.ts
@@ -2,6 +2,8 @@
 import { Injectable } from '@nestjs/common';
 import { EventService } from './common/events/events.service';
 import { CustomLogger } from './common/logger/logger.service';
+import validateEnv from './utils/validate-env';
+import * as path from 'path';
 
 @Injectable()
 export class InitService {
@@ -13,6 +15,9 @@ export class InitService {
   async init() {
     try {
       this.logger.log('Initializing application');
+
+      // Validate .env
+      this.validateEnv();
       await this.connectToDatabase();
 
       this.eventService.emitEvent('ready', this.logger);
@@ -30,5 +35,15 @@ export class InitService {
         resolve(true);
       }, 2000);
     });
+  }
+
+  private validateEnv() {
+    const envPath = path.resolve(__dirname, '../.env');
+    const sampleEnvPath = path.resolve(__dirname, '../.env.sample');
+    const missingVariablesMessage = validateEnv(envPath, sampleEnvPath);
+
+    if (missingVariablesMessage) {
+      throw new Error(missingVariablesMessage);
+    }
   }
 }
