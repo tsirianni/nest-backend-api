2dcd7f61f7cac23a70aba96d5563a97324f3723c
Tobias <sirianni.gf@gmail.com>
2024-06-14 20:05:00 -0300

    chore: add user type data migration

---
+++ prisma/migrations/20240613184901_insert_user_type_data/migration.sql
@@ -0,0 +1,7 @@
+-- Insert User Type Data
+
+INSERT INTO "user_type" (id, name, created_at, updated_at, deleted_at)
+VALUES 
+    (1, 'Technical', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, null),
+    (2, 'Non-technical', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, null);
+
