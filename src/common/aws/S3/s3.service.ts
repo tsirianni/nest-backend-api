9bc12a0265455fa0286e217aa4ed60194ec1e921
Tobias Sirianni Melo <tobias.melo@circularbrain.io>
2025-04-13 11:29:17 -0300

    test: add unit tests for s3 service

--- src/common/aws/S3/s3.service.ts
+++ src/common/aws/S3/s3.service.ts
@@ -1,30 +1,23 @@
 import { DeleteObjectCommand, GetObjectCommand, HeadObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
 import { Upload } from '@aws-sdk/lib-storage';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { EnvSchema } from '../../../config';
-import { Attachment } from '../../attachment/attachment.interceptor';
+
+import type { Attachment } from '../../attachment/attachment.interceptor';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { AmazonS3Exception } from '../../exceptions';
+import { EnvSchema } from '../../../config';
 
 @Injectable()
 export class S3Service {
-  private readonly client: S3Client;
+  private client: S3Client | undefined;
 
-  constructor(private config: ConfigService<EnvSchema, true>) {
-    this.client = new S3Client({
-      region: this.config.get('AWS_REGION'),
-      credentials: {
-        accessKeyId: this.config.get('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: this.config.get('AWS_SECRET_ACCESS_KEY'),
-      },
-    });
-  }
+  constructor(private config: ConfigService<EnvSchema, true>) {}
 
   async uploadS3Object(bucket: string, key: string, file: Attachment): Promise<void> {
     try {
       const s3Upload = new Upload({
-        client: this.client,
+        client: this.getClient(),
         params: {
           Bucket: bucket,
           Key: key,
@@ -42,20 +35,21 @@ export class S3Service {
   async getS3ObjectUrl(bucket: string, key: string): Promise<{ url: string }> {
     let url;
     try {
+      /* Helps to identify if the object exists in the bucket */
       const headCommand = new HeadObjectCommand({
         Bucket: bucket,
         Key: key,
       });
 
-      /* Helps to identify if the object exists in the bucket */
-      await this.client.send(headCommand);
+      const s3Client = this.getClient();
+      await s3Client.send(headCommand);
 
       const command = new GetObjectCommand({
         Bucket: bucket,
         Key: key,
       });
 
-      url = await getSignedUrl(this.client, command, { expiresIn: 60000 }); // 60s
+      url = await getSignedUrl(s3Client, command, { expiresIn: 60000 }); // 60s
     } catch (error) {
       throw new AmazonS3Exception(error);
     }
@@ -64,15 +58,29 @@ export class S3Service {
   }
 
   async deleteS3Object(bucket: string, key: string): Promise<void> {
+    const s3Client = this.getClient();
     try {
       const command = new DeleteObjectCommand({
         Bucket: bucket,
         Key: key,
       });
 
-      await this.client.send(command);
+      await s3Client.send(command);
     } catch (error) {
       throw new AmazonS3Exception(error);
     }
+  }
+
+  private getClient(): S3Client {
+    if (!this.client) {
+      this.client = new S3Client({
+        region: this.config.get('AWS_REGION'),
+        credentials: {
+          accessKeyId: this.config.get('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: this.config.get('AWS_SECRET_ACCESS_KEY'),
+        },
+      });
+    }
+    return this.client;
   }
 }
