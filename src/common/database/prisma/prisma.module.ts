332008a4eae175575245f8005386ef11733bc2bf
Tobias Sirianni Melo <tobias.melo@circularbrain.io>
2025-05-02 18:22:03 -0300

    refactor: Refactor Prisma integration into a global module
    Marked PrismaModule as global to reduce redundancy across modules. Removed explicit imports of PrismaModule and PrismaService across different modules, simplifying dependency management.

--- src/common/database/prisma/prisma.module.ts
+++ src/common/database/prisma/prisma.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './index';
 
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { EmailModule } from '../../common/email/email.module';
-import { PrismaService } from '../../common/database/prisma';
 import { CipherModule } from '../../common/cipher/cipher.module';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService],
   imports: [EmailModule, CipherModule],
   exports: [UsersService],
 })
